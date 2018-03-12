aa26514ce07223083ea5bf349f87970dac2a5b56
Adam Lackorzynski <adam@l4re.org>
2018-03-12 00:00:00 +0000

    Change timestamp format to contain dashes
    Change-Id: Id8869d98c0c0d11c2acb046b655e02d684197593

--- server/src/client.cc
+++ server/src/client.cc
@@ -39,7 +39,7 @@ Client::print_timestamp()
   struct tm *tt = localtime(&t);
   char b[25];
 
-  if (int l = strftime(b, sizeof(b), "[%d %b %y %T] ", tt))
+  if (int l = strftime(b, sizeof(b), "[%Y-%m-%d %T] ", tt))
     wbuf()->put(b, l);
 }
 
