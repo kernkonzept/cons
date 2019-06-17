f330397d6a1d65ca304c8e43330c75af252e565f
Frank Mehnert <frank.mehnert@kernkonzept.com>
2019-06-17 00:00:00 +0000

    mux_impl: prevent copying Mux_i objects by accident
    If such objects would be copied then an adaption of ~Mux_i would be
    required, in particular handling of _self_client.
    
    Change-Id: I764a6f927fb53bd1651006095eabc50f87861687

--- server/src/mux_impl.h
+++ server/src/mux_impl.h
@@ -69,6 +69,8 @@ public:
   char const *name() const { return _name; }
 
 private:
+  Mux_i(Mux_i const &) = delete;
+  Mux_i &operator = (Mux_i const &) = delete;
   typedef cxx::H_list<Frontend> Fe_list;
   typedef Fe_list::Iterator Fe_iter;
   typedef Fe_list::Const_iterator Const_fe_iter;
