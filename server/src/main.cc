07ebc2b77d9ca698c5bc24a0b838b0a85ede1641
Philipp Eppelt <philipp.eppelt@kernkonzept.com>
2025-10-20 00:00:00 +0000

    fix second part of client index assignment
    I7ac7f623b415e62d017a5bc610439e33b210eccf improved the counting,
    however, in case the user drops a client, the index is duplicated
    again.
    The original behavior was:
    1. forward scan the client list for a client with duplicate tag,
    2. assign the new client the found client's index plus 1,
    3. insert the new client before the found client.
    
    Since the client list is a vector now and we only add elements to the
    back of the vector, we do the above with a reverse search to
    reinstate the original behavior.
    1. reverse scan the client list for aclient with duplicated tab,
    2. assign the new client the found client's index plus 1,
    3. inser the new client after the found client.
    
    Change-Id: I81c188273dd86dcbcc149f3a0d06319295dac639

--- server/src/main.cc
+++ server/src/main.cc
@@ -153,9 +153,9 @@ Cons_svr::create(std::string const &tag, int color, CLI **vout, size_t bufsz,
 
   std::string name = tag.length() > 0 ? tag : "<noname>";
 
-  unsigned cnt =
-    std::count_if(_ctl.clients.begin(), _ctl.clients.end(),
-                  Client::Equal_tag(cxx::String(name.data(), name.length())));
+  auto it =
+    std::find_if(_ctl.clients.rbegin(), _ctl.clients.rend(),
+                 Client::Equal_tag(cxx::String(name.data(), name.length())));
 
   CLI *v = new CLI(name, color, bufsz, key, line_buffering, line_buffering_ms,
                    &registry, &server, &_ctl);
@@ -168,10 +168,10 @@ Cons_svr::create(std::string const &tag, int color, CLI **vout, size_t bufsz,
       return -L4_ENOMEM;
     }
 
-  if (cnt > 0)
+  if (it != _ctl.clients.rend())
     {
       sys_msg("WARNING: multiple clients with tag '%s'\n", name.c_str());
-      v->idx = cnt;
+      v->idx = (*it)->idx + 1;
       _ctl.clients.push_back(v);
     }
   else
